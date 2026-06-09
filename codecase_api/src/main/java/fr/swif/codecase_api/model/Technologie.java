41721431189cd6b9e64c3fb1d8951824b9df7971
Alexandre Calderoli <alexandrecalderoli96@gmail.com>
2026-06-09 16:17:01 +0200

    <FEAT> Création et complétion de la classe Technologie

---
+++ codecase_api/src/main/java/fr/swif/codecase_api/model/Technologie.java
@@ -0,0 +1,50 @@
+package fr.swif.codecase_api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+/**
+ * Technologie
+ * <i>de fr.swif.codecase_api.model</i>
+ * <hr>
+ * <p></p>
+ *
+ * @author Calderoli Alexandre
+ * @version 0.0.1
+ * @since 09/06/2026
+ */
+
+@Data
+@Entity
+@Table(name = "Technologie")
+public class Technologie {
+  /**
+   * Variable technologieId
+   */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "technologieId")
+  private Integer technologieId;
+
+  /**
+   * Variable technologieIntitule
+   */
+  @NotBlank
+  @Column(name = "technologieIntitule")
+  private String technologieIntitule;
+
+  /**
+   * Variable userId de la classe User
+   */
+  @ManyToOne
+  @JoinColumn(name = "userId")
+  private User userId;
+}
