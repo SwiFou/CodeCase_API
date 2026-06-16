d45761c9d672eff5a7d5aaa74b1f222a079aaa39
Alexandre Calderoli <alexandrecalderoli96@gmail.com>
2026-06-16 14:43:09 +0200

    <FIX> Changement de l'annotations dans la classe User de userRole -> @NotNull

--- codecase_api/src/main/java/fr/swif/codecase_api/model/User.java
+++ codecase_api/src/main/java/fr/swif/codecase_api/model/User.java
@@ -71,7 +71,7 @@ public class User {
   /**
    * Variable userRole
    */
-  @NotEmpty
+  @NotNull
   @Enumerated(EnumType.STRING) // Permet de stocker
                               // "VISITEUR"/"USER"/"MODO"/"ADMIN" au lieu de 0/1
   @Column(name = "userRole")
